ce900b403b24991af3cbda3964f2912ee2a320f1
ireneterpstra <ireneterpstra2@gmail.com>
2018-09-29 15:26:12 -0400

    Pendelum + piano vibrate

---
+++ PendelumNeoLED/ChaseRainbow.ino
@@ -0,0 +1,41 @@
+void theaterChaseRainbow(int SpeedDelay) {
+  byte *c;
+  
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < numLeds; i=i+3) {
+          c = Wheel( (i+j) % 255);
+          strip.setPixelColor(i+q, *c, *(c+1), *(c+2));    //turn every third pixel on
+        }
+        strip.show();
+       
+        delay(SpeedDelay);
+       
+        for (int i=0; i < numLeds; i=i+3) {
+          strip.setPixelColor(i+q, 0,0,0);        //turn every third pixel off
+        }
+    }
+  }
+}
+
+byte * Wheel(byte WheelPos) {
+  static byte c[3];
+  
+  if(WheelPos < 85) {
+   c[0]=WheelPos * 3;
+   c[1]=255 - WheelPos * 3;
+   c[2]=0;
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   c[0]=255 - WheelPos * 3;
+   c[1]=0;
+   c[2]=WheelPos * 3;
+  } else {
+   WheelPos -= 170;
+   c[0]=0;
+   c[1]=WheelPos * 3;
+   c[2]=255 - WheelPos * 3;
+  }
+
+  return c;
+}
--- Vibrate2/Vibrate2.ino
+++ Vibrate2/Vibrate2.ino
@@ -1,8 +1,3 @@
-/*
- * Irene Terpstra
- * 7/21/2018 
- * Vibrator controled by IMU
- */
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -41,7 +36,7 @@ double BZ;
 
 int BNO055_SAMPLERATE_DELAY_MS = 100;
 
-Adafruit_BNO055 bno = Adafruit_BNO055(56, (0x29));
+//Adafruit_BNO055 bno = Adafruit_BNO055(55, (0x29));
 
 
 float calculateResistance(int pinout){
@@ -60,67 +55,67 @@ float calculateResistance(int pinout){
     sensor API sensor_t type (see Adafruit_Sensor for more information)
 */
 /**************************************************************************/
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  bno.getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:    ")); Serial.println(sensor.max_value);
-  Serial.print  (F("Min Value:    ")); Serial.println(sensor.min_value);
-  Serial.print  (F("Resolution:   ")); Serial.println(sensor.resolution);
-  Serial.println(F("------------------------------------"));
-  Serial.println();
-  delay(500);
-}
+//void displaySensorDetails(void)
+//{
+//  sensor_t sensor;
+//  bno.getSensor(&sensor);
+//  Serial.println(F("------------------------------------"));
+//  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+//  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+//  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+//  Serial.print  (F("Max Value:    ")); Serial.println(sensor.max_value);
+//  Serial.print  (F("Min Value:    ")); Serial.println(sensor.min_value);
+//  Serial.print  (F("Resolution:   ")); Serial.println(sensor.resolution);
+//  Serial.println(F("------------------------------------"));
+//  Serial.println();
+//  delay(500);
+//}
 
 /**************************************************************************/
 /*
     Display sensor calibration status
 */
 /**************************************************************************/
-void displayCalStatus(void)
-{
-  /* Get the four calibration values (0..3) Any sensor data reporting 0 should be ignored, 3 means 'fully calibrated" */
-  uint8_t system, gyro, accel, mag;
-  system = gyro = accel = mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-
-  /* The data should be ignored until the system calibration is > 0 */
-  Serial.print(F("\t"));
-  if (!system)
-  {
-    Serial.print(F("! "));
-  }
-}
-
-void updateSensorData(){
-  sensors_event_t event;
-  bno.getEvent(&event);
-
-  angle1 = calculateResistance(FLEX_PIN1);
-  angle2 = calculateResistance(FLEX_PIN2);
-  
-  if (angle2 > 50){
-    zeroBX = event.orientation.x;
-  }
-  if ((event.orientation.x - zeroBX) > 0){
-    tempBX = event.orientation.x - zeroBX;
-  } else {
-    tempBX = 360 + (event.orientation.x - zeroBX);
-  }
-  
-  if(tempBX <= 180){
-    BX = tempBX;
-  } else {
-    BX = tempBX - 360;
-  }
-  
-  BZ = event.orientation.z;
-  BY = event.orientation.y;
-}
+//void displayCalStatus(void)
+//{
+//  /* Get the four calibration values (0..3) Any sensor data reporting 0 should be ignored, 3 means 'fully calibrated" */
+//  uint8_t system, gyro, accel, mag;
+//  system = gyro = accel = mag = 0;
+//  bno.getCalibration(&system, &gyro, &accel, &mag);
+//
+//  /* The data should be ignored until the system calibration is > 0 */
+//  Serial.print(F("\t"));
+//  if (!system)
+//  {
+//    Serial.print(F("! "));
+//  }
+//}
+//
+//void updateSensorData(){
+//  sensors_event_t event;
+//  bno.getEvent(&event);
+//
+//  angle1 = calculateResistance(FLEX_PIN1);
+//  angle2 = calculateResistance(FLEX_PIN2);
+//  
+//  if (angle2 > 50){
+//    zeroBX = event.orientation.x;
+//  }
+//  if ((event.orientation.x - zeroBX) > 0){
+//    tempBX = event.orientation.x - zeroBX;
+//  } else {
+//    tempBX = 360 + (event.orientation.x - zeroBX);
+//  }
+//  
+//  if(tempBX <= 180){
+//    BX = tempBX;
+//  } else {
+//    BX = tempBX - 360;
+//  }
+//  
+//  BZ = event.orientation.z;
+//  BY = event.orientation.y;
+//}
 
 //---------------------------------------------------------------------------------------------------------
 //  Setup
@@ -131,44 +126,58 @@ void setup(void)
   Serial.println(F("Orientation Sensor Test")); Serial.println();
 
   /* Initialise the sensor */
-  if(!bno.begin())
-  {
-    Serial.print(F("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!"));
-    while(1);
-  }
-  delay(500);
-
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  bno.setExtCrystalUse(true);
+//  if(!bno.begin())
+//  {
+//    Serial.print(F("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!"));
+//    while(1);
+//  }
+//  delay(500);
+//
+//  /* Display some basic information on this sensor */
+//  displaySensorDetails();
+//  bno.setExtCrystalUse(true);
   
   pinMode(6, OUTPUT);
   pinMode(FLEX_PIN1, INPUT);
   pinMode(FLEX_PIN2, INPUT);
 
 }
+
 //---------------------------------------------------------------------------------------------------------
 //  Loop
 //---------------------------------------------------------------------------------------------------------
 
 void loop(void) {
 //  Serial.println(F("Loop"));
+//  sensors_event_t event;
+////  bno.getEvent(&event);
+////  BX = event.orientation.x;
+////  BZ = event.orientation.z;
+////  BY = event.orientation.y;
+//  
+//  updateSensorData();
+//
   sensors_event_t event;
 //  bno.getEvent(&event);
-//  BX = event.orientation.x;
-//  BZ = event.orientation.z;
-//  BY = event.orientation.y;
-  
-  updateSensorData();
+
+  angle1 = calculateResistance(FLEX_PIN1);
+  angle2 = calculateResistance(FLEX_PIN2);
 
 
-  if ((BZ - BY) < 10 && (BZ - BY > -10)){ //Pitch
+  if (angle1 > 20 || angle2 > 20){ //Pitch
     analogWrite(6, 153);
-    //Serial.print("Angle 1: " + String(angle1) + " Angle 2: " + String(angle2) + "  ");
-    Serial.print("Pitch: " + String(BZ) + "  ");
-    Serial.print("Yaw: " + String(BX) + "  ");
-    Serial.println("Roll: " + String(BY) + " : vibrator on");
+    Serial.println("Angle 1: " + String(angle1) + " Angle 2: " + String(angle2) + "  ");
+//    Serial.print("Pitch: " + String(BZ) + "  ");
+//    Serial.print("Yaw: " + String(BX) + "  ");
+//    Serial.println("Roll: " + String(BY) + " : vibrator on");
   } 
+//  if ((BZ - BY) < 10 && (BZ - BY > -10)){ //Pitch
+//    analogWrite(6, 153);
+//    //Serial.print("Angle 1: " + String(angle1) + " Angle 2: " + String(angle2) + "  ");
+//    Serial.print("Pitch: " + String(BZ) + "  ");
+//    Serial.print("Yaw: " + String(BX) + "  ");
+//    Serial.println("Roll: " + String(BY) + " : vibrator on");
+//  } 
 //  else if((BX < 10) || (BX > -10)){ //Yaw
 //    analogWrite(6, 153);
 //    //Serial.print("Angle 1: " + String(angle1) + " Angle 2: " + String(angle2) + "  ");
@@ -184,10 +193,10 @@ void loop(void) {
 //  } 
   else {
     analogWrite(6, 0);
-    //Serial.print("Angle 1: " + String(angle1) + " Angle 2: " + String(angle2) + "  ");
-    Serial.print("Pitch: " + String(BZ) + "  ");
-    Serial.print("Yaw: " + String(BX) + "  ");
-    Serial.println("Roll: " + String(BY) + " : vibrator off");
+    Serial.println("Angle 1: " + String(angle1) + " Angle 2: " + String(angle2) + "  ");
+//    Serial.print("Pitch: " + String(BZ) + "  ");
+//    Serial.print("Yaw: " + String(BX) + "  ");
+//    Serial.println("Roll: " + String(BY) + " : vibrator off");
   }
 
 }
